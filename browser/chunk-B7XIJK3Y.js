acb4c5fb0068fea19b54fb4cf4114fb3794ac0e6
Samu <111547859+STrianav@users.noreply.github.com>
2025-05-29 21:33:44 -0500

    Add files via upload

---
+++ browser/chunk-B7XIJK3Y.js
@@ -0,0 +1,1 @@
+import{Fb as r,Xa as n,ob as o,pb as i}from"./chunk-RCD2EZCC.js";var s=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-register"]],decls:2,vars:0,template:function(e,p){e&1&&(o(0,"p"),r(1,"register works!"),i())},encapsulation:2})};export{s as RegisterComponent};
